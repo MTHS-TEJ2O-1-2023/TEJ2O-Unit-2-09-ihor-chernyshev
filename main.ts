5d967ece11fd652e8467c2f8d67cc6fc030b0636
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-10 13:28:36 -0400

    adsf

--- main.ts
+++ main.ts
@@ -1,8 +1,45 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Ihor Chernyshev
+ * Created on: Oct 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let randomNumber: number = 0
+
+randomNumber = -1
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+  randomNumber = randint(0, 2)
+  basic.clearScreen()
+  
+  if (randomNumber == 0) {
+    images.createImage(`
+      . . . . .
+      . # # # .
+      . # # # .
+      . # # # .
+      . . . . .
+    `).showImage(0);
+    }
+    else if (randomNumber == 1){
+      images.createImage(`
+      # # # # #
+      # . . . #
+      # . . . #
+      # . . . #
+      # # # # #
+    `).showImage(0);
+    }
+    else if (randomNumber == 2) {
+      images.createImage(`
+      # # # # #
+      # . . . #
+      # . . . #
+      # . . . #
+      # # # # #
+    `).showImage(0);
+    }
+})
