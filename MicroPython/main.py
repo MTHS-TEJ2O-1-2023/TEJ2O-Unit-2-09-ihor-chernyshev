cd9ed6bc207858cc9bc221c04fdde80694c5a57e
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-10-11 19:57:17 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -15,7 +15,7 @@ display.show(Image.HAPPY)
 
 while True:
     if accelerometer.was_gesture("shake"):
-        random_number = random.randint(0, 0)
+        random_number = random.randint(0, 2)
         display.clear()
 
         if random_number == 0:
@@ -24,26 +24,26 @@ while True:
             display.clear()
             display.show(Image.HAPPY)
 
-        elif random_number == 1:
+        if random_number == 1:
             display.show(Image("99999:" "90009:" "90009:" "90009:" "99999"))
             sleep(2000)
             display.clear()
             display.show(Image.HAPPY)
 
-        elif random_number == 2:
+        if random_number == 2:
             display.show(Image("99009:" "99090:" "00900:" "99090:" "99009"))
             sleep(2000)
             display.clear()
             display.show(Image.HAPPY)
 
-        if button_a.was_pressed():
-            display.clear()
-            score = score + 1
-            display.show(Image.YES)
-            sleep(1)
-            display.show(Image.HAPPY)
+    if button_a.was_pressed():
+        score = score + 1
+        display.clear()
+        display.show(Image.YES)
+        sleep(1000)
+        display.show(Image.HAPPY)
 
-        if button_b.was_pressed():
-            display.clear()
-            display.scroll("Score : " + str(score))
-            display.show(Image.HAPPY)
+    if button_b.was_pressed():
+        display.clear()
+        display.scroll("Score : " + str(score))
+        display.show(Image.HAPPY)
